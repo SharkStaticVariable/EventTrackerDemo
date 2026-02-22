9ace05359f02929baad8016e9481b522cb7679d7
Дэнчик <arapov34denis@gmail.com>
2026-02-22 20:38:23 +0300

    /

---
+++ src/main/java/io/project/eventtrackerdemo/EventTrackerDemoApplication.java
@@ -0,0 +1,13 @@
+package io.project.eventtrackerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventTrackerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventTrackerDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/project/eventtrackerdemo/EventTrackerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.project.eventtrackerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventTrackerDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
